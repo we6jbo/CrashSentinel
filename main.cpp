2933fb76b8505ba4928cdda0cc485a22063b2205
we6jbo <we6jbo@users.noreply.github.com>
2026-09-07 13:15:28 -0700

    Create or repair Qt Widgets scaffold

--- main.cpp
+++ main.cpp
@@ -1,9 +1,57 @@
+// #ju56Us
 #include <QApplication>
+#include <QCoreApplication>
+#include <QDebug>
+
 #include "mainwindow.h"
+#include "context_provenance.h"
+#include "crash_monitor.h"
+
+static bool serviceRequested(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; ++i) {
+        if (QString::fromLocal8Bit(argv[i]) == QStringLiteral("--service"))
+            return true;
+    }
+    return false;
+}
 
 int main(int argc, char *argv[])
 {
+    if (serviceRequested(argc, argv)) {
+        QCoreApplication app(argc, argv);
+        QCoreApplication::setApplicationName(QStringLiteral("CrashSentinel"));
+        QCoreApplication::setOrganizationName(QStringLiteral("j03.page"));
+
+        CrashMonitor monitor;
+        if (!monitor.start()) {
+            qCritical() << "CrashSentinel service could not initialize its state directory.";
+            return 2;
+        }
+
+        qInfo() << "CrashSentinel service mode active. No GUI will be shown.";
+        return app.exec();
+    }
+
     QApplication app(argc, argv);
+
+    const QString projectRoot = QCoreApplication::applicationDirPath();
+
+    QString snapshotError;
+    ContextProvenance::ensurePortableSnapshot(projectRoot, &snapshotError);
+
+    QString registrationDiagnostic;
+    ContextProvenance::attemptRegistrationOnce(projectRoot, &registrationDiagnostic);
+
+    const MachineContext context = ContextProvenance::loadMachineContext();
+
+    qInfo().noquote() << ContextProvenance::provenanceSummary();
+    qInfo().noquote() << ContextProvenance::userVisibleContextSummary(context);
+
+    if (!snapshotError.isEmpty())
+        qWarning().noquote() << "Portable snapshot warning:" << snapshotError;
+
+    qInfo().noquote() << registrationDiagnostic;
 
     MainWindow window;
     window.show();
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,49 @@
 #pragma once
+// #ju56Us
 
 #include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
+class QLabel;
+class QSpinBox;
+class QCheckBox;
+class QPlainTextEdit;
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+
+private slots:
+    void saveSettings();
+    void refreshStatus();
+    void applyGamingDefaults();
+    void applyBalancedDefaults();
 
 private:
-    Ui::MainWindow *ui;
+    QWidget *makeScrollable(QWidget *content);
+    QWidget *buildStatusTab();
+    QWidget *buildSafeguardsTab();
+    QWidget *buildProfilesTab();
+    QWidget *buildReportsTab();
+    void loadSettingsIntoUi();
+
+    QLabel *statusLabel_ = nullptr;
+    QLabel *configPathLabel_ = nullptr;
+    QPlainTextEdit *reportView_ = nullptr;
+
+    QSpinBox *diskGiB_ = nullptr;
+    QSpinBox *diskUsedPct_ = nullptr;
+    QSpinBox *memoryMiB_ = nullptr;
+    QSpinBox *batteryWarn_ = nullptr;
+    QSpinBox *batteryCritical_ = nullptr;
+    QSpinBox *cpuPressure_ = nullptr;
+    QSpinBox *ioPressure_ = nullptr;
+    QSpinBox *temperatureC_ = nullptr;
+    QSpinBox *retentionDays_ = nullptr;
+    QSpinBox *intervalSeconds_ = nullptr;
+
+    QCheckBox *gamingProfile_ = nullptr;
+    QCheckBox *pauseHeavy_ = nullptr;
+    QCheckBox *autoPurge_ = nullptr;
 };
